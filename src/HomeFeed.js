587ac90f4c49b39de53a076e00761fc6136b2d5c
birthquake <rashiedtyre@gmail.com>
2025-07-28 12:00:34 -0400

    Update HomeFeed.js

--- src/HomeFeed.js
+++ src/HomeFeed.js
@@ -515,7 +515,7 @@ const HomeFeed = ({ photos, currentUser }) => {
             justifyContent: "flex-start",
             alignItems: "center",
             gap: "2px",
-            padding: "0 16px",
+            padding: "0 12px",
           }}
         >
           {/* Global/Local Toggle - Hide for Discovery tab */}
